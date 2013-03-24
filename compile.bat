0684182b7462c119c6930c36759c0f96047eb50a
NovakGergely <novak.r.gergely@gmail.com>
2013-03-24 18:30:17 +0100

    scriptek javitasa

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @echo off
 IF NOT exist bin (mkdir bin)
 set /p java_home="Enter the path of the java JDK: "
-set JAVA_HOME=java_home
+set JAVA_HOME=%java_home%
 set java_path=%java_home%/bin
-set PATH=%PATH%;%java_path
+set PATH=%PATH%;%java_path%
 javac -d bin src/modell/*.java src/skeleton/*.java
