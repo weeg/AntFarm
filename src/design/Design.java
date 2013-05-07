70ecb7d13350bc124b3c80a6e3108fb8863fd10b
NovakGergely <novak.r.gergely@gmail.com>
2013-05-07 14:08:54 +0200

    Design

--- src/design/Design.java
+++ src/design/Design.java
@@ -1,7 +1,13 @@
 package design;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import view.GladeView;
-import view.pc.PcUI;
+import view.View;
 import modell.Glade;
 
 public class Design {
@@ -11,11 +17,24 @@ public class Design {
 	 */
 	public static void main(String[] args) {
 		
-		
-		
-		
-		new PcUI();
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });		
 		
 	}
+	
+	private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Ant Farm");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
 
 }
